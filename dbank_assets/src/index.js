4f2836d3577c10fb74f382f7ab36a9121311259d
Ronin@73 <113249077+AtulRaghuvanshi73@users.noreply.github.com>
2023-05-03 11:45:34 +0530

    Add files via upload

---
+++ dbank_assets/src/index.js
@@ -0,0 +1,37 @@
+import {dbank} from "../../declarations/dbank"
+
+window.addEventListener("load", async function(){
+  update();
+});
+
+document.querySelector("form").addEventListener("submit",async function(event){
+  event.preventDefault();
+
+  const button = event.target.querySelector("Submit-btn");
+    //console.log("Submitted.");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled",true);
+
+  if (document.getElementById("input-amount").value.length != 0){
+   await dbank.topUp(inputAmount);
+  }
+
+  if(document.getElementById("withdrawal-amount").value.length != 0){
+    await dbank.withdrawl(outputAmount);
+  }
+
+  update();
+
+  document.getElementById("input-amount").value = "";
+  doucment.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled");
+});
+
+async function update(){
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText =Math.round(currentAmount * 100)/100;
+}
